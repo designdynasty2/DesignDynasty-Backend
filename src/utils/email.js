99159dc6ab9a1cb90b23e1b8a81973cbe92e2ffd
MATHIAZHAGAN MS <146163288+Mathi-MS@users.noreply.github.com>
2025-09-26 18:45:08 +0530

    Update email.js

--- src/utils/email.js
+++ src/utils/email.js
@@ -16,6 +16,7 @@ export async function getTransport() {
   const transport = nodemailer.createTransport({
     host: "smtp.ethereal.email",
     port: 587,
+    secure: false,
     auth: { user: testAccount.user, pass: testAccount.pass },
   });
   console.log("Using Ethereal test SMTP");
@@ -46,3 +47,4 @@ export async function sendEmail({ to, subject, text, html, attachments = [] }) {
 
   return info;
 }
+
